f860179ebfe9518a05db0a949b9b648f8b278506
ayushagarwal2502 <159728868+ayushagarwal2502@users.noreply.github.com>
2025-01-20 09:52:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
   // your name here
+const makeChange = (c) => {
+  let quarters = Math.floor(c / 25);
+  c = c % 25;
+
+  let dimes = Math.floor(c / 10);
+  c = c % 10;
+
+  let nickels = Math.floor(c / 5);
+  let pennies = c % 5;
+
+  return { "q": quarters, "d": dimes, "n": nickels, "p": pennies };
+};
+	
 };
 
 // Do not the change the code below
